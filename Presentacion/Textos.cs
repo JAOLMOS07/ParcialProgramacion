74bcda3adb309fa5c4c53f216908ab4254715207
Daniel Padilla <Daniel Padilla@LAPTOP-KOPLH686>
2023-03-27 21:16:32 -0500

    Daniel5

---
+++ Presentacion/Textos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion
+{
+    public class Textos
+    {
+        public string MenuPrincipal()
+        {
+            Console.WriteLine();
+            var msg = "----------Menu----------";
+            String[] strings = { "Liquidaciones Licores,vinos o aperitivos", "Liquidaciones Cervezas" };
+            
+            for (int i=0; i<strings.Count(); i++) 
+            {
+                msg+= (i+1) + strings[i]+ "\n";
+            }
+            return msg;
+        }
+    }
+}
